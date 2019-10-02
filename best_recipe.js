a11f3664e80830762e1de6a43b8f1c97b35fc39b
Nargiza O <nargiza2k@gmail.com>
2019-10-02 10:04:36 -0500

    Fixed responsive website

--- best_recipe.js
+++ best_recipe.js
@@ -3,8 +3,6 @@ $.ajax({
     url: "http://quotes.rest/qod.json?category=inspire",
     dataType: "JSON",
     success: function(response){
-    //   var dailyQuote=(response.contents.quotes[0]);
-    //   return dailyQuote;
     console.log(response);
     text(response.contents.quotes[0].quote, response.contents.quotes[0].author);
     },
@@ -12,13 +10,14 @@ $.ajax({
       console.log("Error: " + error);
     }
    });
-  /*function quotes(){
- var quotesPosition=document.getElementsByTagName("quotes");
- quotesPosition.(dailyQuote);
-  }*/
+ 
   function text(argument,argument1){
  var heading=$("<h3/>").text(' " ' + argument + ' \" ');
  $("#quotes").append(heading);
  var author=$("<h5/>").text(argument1);
  $("#author").append(author);
+  }
+
+  function(){
+    var button1 = document.getElementById("button").innerHTML = "<div></div>"
   }
