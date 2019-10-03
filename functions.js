c2402140482c63701736a630c78580c7b88c2f33
Patrick Moore <patrickwmoore88@gmail.com>
2019-10-03 10:05:28 -0500

    Adjusted the calendar page with the images and recipes on the main screen

--- functions.js
+++ functions.js
@@ -40,22 +40,114 @@ $.ajax({
   dataType: "JSON",
   success: function(response){
   console.log(response);
-  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader1, myModalBody1, myModalImage1);
   },
   error: function(error){
     console.log("Error: " + error);
   }
   });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader2, myModalBody2, myModalImage2 );
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader3, myModalBody3, myModalImage3);
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader4, myModalBody4, myModalImage4);
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader5, myModalBody5, myModalImage5);
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader6, myModalBody6, myModalImage6);
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+$.ajax({
+  type: "GET",
+  url: "https://www.themealdb.com/api/json/v1/1/random.php",
+  dataType: "JSON",
+  success: function(response){
+  console.log(response);
+  foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader7, myModalBody7, myModalImage7);
+  },
+  error: function(error){
+    console.log("Error: " + error);
+  }
+  });
+
+
   
-  function foodData(arg, arg1, arg3){
-      var meal=$(".name").text(arg);
-      $(".name").append(meal);
-      var recipe =$(".modal-body").text(arg1);
-      $(".modal-body").append(recipe);
+  function foodData(arg, arg1, arg3, headerId, bodyId, imageId){
+      var meal=$(headerId).text(arg);
+      $(headerId).append(meal);
+      var recipe =$(bodyId).text(arg1);
+      $(bodyId).append(recipe);
 
      var image = document.createElement("img")
-      //image.innerHTML="<img src=" +arg3+ "></img>";
-      $(".modal-body").append(image.innerHTML="<img src=" +arg3+ "></img>");
-      
-      
+      $(imageId).append(image.innerHTML="<img src=" +arg3+ "></img>");   
    }
+
+   var buttonElement1 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader1, myModalBody1, myModalImage1));
+    var buttonElement2 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader2, myModalBody2, myModalImage2));
+    var buttonElement3 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader3, myModalBody3, myModalImage3));
+    var buttonElement4 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader4, myModalBody4, myModalImage4));
+    var buttonElement5 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader5, myModalBody5, myModalImage5));
+    var buttonElement6 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader6, myModalBody6, myModalImage6));
+    var buttonElement7 = document.getElementById('today');
+    buttonElement1.addEventListener('click',foodData(response.meals[0].strMeal, response.meals[0].strInstructions, response.meals[0].strMealThumb, myModalHeader7, myModalBody7, myModalImage7));
